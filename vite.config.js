6cf7ffdf3c90de9e6f7e6ad735587e9a2ebe5787
mzhukov <mzhukov@aoiot.ru>
2025-03-04 16:05:05 +0800

    replace fonts

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgLoader from 'vite-svg-loader';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
+  base: '/',
   css: {
     preprocessorOptions: {
       scss: {
